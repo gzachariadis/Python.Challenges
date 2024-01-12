9db363d0b60c89b4717cb0212f0e985f5ee7aa88
gzachariadis <67559421+gzachariadis@users.noreply.github.com>
2024-01-12 14:17:00 +0200

    Making progress on add and update

--- add.py
+++ add.py
@@ -1,6 +1,8 @@
 import requests
 import sys
-import json
+from os.path import dirname, abspath
+import os
+import re
 
 # Required Inputs
 
@@ -8,6 +10,7 @@ import json
 # - Platform
 # - Username
 
+Disallowed_Characters = r'[\\/*?:"<>|]'
 
 # The API endpoint
 codewars_url = "https://www.codewars.com/api/v1/code-challenges/" + sys.argv[1]
@@ -18,12 +21,34 @@ response = requests.get(codewars_url)
 # Print the response
 response_json = response.json()
 
-Challenge_Name = response_json['name']
-Challenge_URL = response_json['url']
-Challenge_Rank = response_json['rank']['name']
+Challenge_Name = re.sub(Disallowed_Characters,"",  response_json['name'])
+Challenge_URL = str(response_json['url']).strip()
+Challenge_Rank = str(re.sub(' ','-', response_json['rank']['name'])).strip()
+
+# Repository Directory
+current_d = dirname(abspath(__file__))
+Platform = sys.argv[2]
+
+full_path = current_d + '\{}'.format(Platform) + '\{}'.format(Challenge_Rank) + '\{}'.format(Challenge_Name)
+
+if not os.path.exists(full_path):
+    os.makedirs(full_path)
+
+    try:
+
+        solution = open(full_path + '\{}'.format("solution.py") , 'w')
+        solution.close()
+        notes = open(full_path + '\{}'.format("notes.txt") , 'w')
+        notes.close()
+        readme = open(full_path + '\{}'.format("README.md") , 'w')
+        readme.close()
+
+        print("New entry was added Successfully.")
+    except FileNotFoundError as e:
+        print(f'An error occurred: {e}')
 
 
+else:
+    print("Challenge Exists Already.")
 
-print(Challenge_Name)
-print(Challenge_URL)
-print(Challenge_Rank)
+
